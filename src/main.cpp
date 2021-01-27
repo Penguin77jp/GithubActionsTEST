52f44f137973b5e45131b46fd39b69d6c69c747e
Penguin_Macbook <koutei7penguin.jp@gmail.com>
2021-01-27 16:34:01 +0900

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+// #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#include <vector>
 
 int main()
 {
+    std::vector<unsigned char> data(100 * 100 * 3, 255);
+    stbi_write_png(data.data(), 100, 100, 3);
     std::cout << "Hello World" << std::endl;
     return 0;
 }
